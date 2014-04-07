f74eb2c69b659098f8c6c375167ff834a76624b7
Abel E <absdrmr81@yahoo.com>
2014-04-07 18:23:53 -0500

    first commit

---
+++ Testing/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Testing
+//
+//  Created by Drmrboy on 4/7/14.
+//  Copyright (c) 2014 Esco, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Testing/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Testing
+//
+//  Created by Drmrboy on 4/7/14.
+//  Copyright (c) 2014 Esco, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
